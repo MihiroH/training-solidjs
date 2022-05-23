b4552f2b6cf7c3e374cc4fb4e1989cae80e15506
hh-pd <hiromiti999@gmail.com>
2022-05-24 02:37:40 +0900

    Fix: リファクタリング

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 (() => {
-  const targets = document.getElementsByClassName('less-than-one-hour')
-  const OPEND_CLASSNAME = 'opend'
+	const targets = document.getElementsByClassName("less-than-one-hour");
+	const OPEN_CLASSNAME = "open";
 
-  for(let target = 0; target < targets.length; target++) {
-    const element = targets[target]
-    element.addEventListener('click', (e) => toggleCard(e, element))
-  }
+	for (let target = 0; target < targets.length; target++) {
+		const element = targets[target];
+		element.addEventListener("click", (e) => toggleCard(e, element));
+	}
 
-  const toggleCard = (e, element) => {
-    const isOpened = element.classList.contains(OPEND_CLASSNAME)
-    if(isOpened) element.classList.remove(OPEND_CLASSNAME)
-    if(!isOpened) element.classList.add(OPEND_CLASSNAME)
-  }
-})()
+	const toggleCard = (e, element) => {
+		const isOpened = element.classList.contains(OPEN_CLASSNAME);
+		if (isOpened) element.classList.remove(OPEN_CLASSNAME);
+		if (!isOpened) element.classList.add(OPEN_CLASSNAME);
+	};
+})();
