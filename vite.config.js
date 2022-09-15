33fd6bd5aa9a70ae2def51b749e931e8087cbd4b
MihiroH <mihiro.hashimoto.1125@gmail.com>
2022-09-16 05:06:16 +0900

    feat: googleフォントを追加

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import path from 'path'
 
 import { defineConfig } from 'vite'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Anton',
+          },
+          {
+            name: 'Noto Sans JP',
+            styles: 'wght@500;700',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
