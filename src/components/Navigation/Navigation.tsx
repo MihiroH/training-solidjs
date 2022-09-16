1d0e878bfe8ae633a3cb927ab7b15ce648160daa
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-09-17 02:03:43 +0900

    fix: ナビゲーションコーディング完了

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,22 @@
+import { For } from 'solid-js'
+
 import styles from '@/components/Navigation/Navigation.module.scss'
+import { links } from '@/constants/index'
 
 const Navigation = () => {
-  return <div class={styles.navigation}>navigation</div>
+  return (
+    <div class={styles.navigation}>
+      <div class={styles.container}>
+        <For each={links}>
+          {(link) => (
+            <a href={link.URL} class={styles.item}>
+              {link.name}
+            </a>
+          )}
+        </For>
+      </div>
+    </div>
+  )
 }
 
 export default Navigation
---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+export const links = [
+  {
+    name: 'TOP',
+    URL: '',
+  },
+  {
+    name: '給与',
+    URL: '',
+  },
+  {
+    name: '特徴',
+    URL: '',
+  },
+  {
+    name: 'サポート',
+    URL: '',
+  },
+  {
+    name: 'スケジュール',
+    URL: '',
+  },
+  {
+    name: '配送エリア',
+    URL: '',
+  },
+  {
+    name: '業務内容',
+    URL: '',
+  },
+  {
+    name: '採用の流れ',
+    URL: '',
+  },
+  {
+    name: '職場体験',
+    URL: '',
+  },
+]
+
+export default {
+  links,
+}
