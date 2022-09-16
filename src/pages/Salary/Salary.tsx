9b64884384534e52e8a71db8a06bf0ca919820c1
MihiroH <mihiro.hashimoto.1125@gmail.com>
2022-09-17 01:04:36 +0900

    feat: ページトップコンポーネントを追加

--- src/pages/Salary/Salary.tsx
+++ src/pages/Salary/Salary.tsx
@@ -1,7 +1,12 @@
+import PageTop from '@/components/PageTop/PageTop'
 import styles from '@/pages/Salary/Salary.module.scss'
 
 const Salary = () => {
-  return <div class={styles.salary}>Salary</div>
+  return (
+    <div class={styles.salary}>
+      <PageTop />
+    </div>
+  )
 }
 
 export default Salary
