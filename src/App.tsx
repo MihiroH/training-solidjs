cc1c8dd3aa69210c87b48d0f6943b7690626ad7b
MihiroH <mihiro.hashimoto.1125@gmail.com>
2022-09-18 04:10:06 +0900

    refactor: セクションヘッダーコンポーネントをリファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { createMemo, Show } from 'solid-js'
 import styles from '@/App.module.scss'
 import KeyVisual from '@/components/KeyVisual/KeyVisual'
 import SectionHeader from '@/components/SectionHeader/SectionHeader'
-import { pagesInfo } from '@/constants/pagesInfo'
+import { PageInfo, pagesInfo } from '@/constants/pagesInfo'
 import { routes } from '@/router'
 import { trailingSlash } from '@/utils'
 
@@ -12,10 +12,10 @@ function App() {
   const Routes = useRoutes(routes)
   const location = useLocation()
   const pathname = createMemo(() => trailingSlash(location.pathname))
-  const currentPageInfo = createMemo(() => pagesInfo.filter((info) => info.path === pathname())[0])
+  const currentPageInfo = createMemo(() => pagesInfo.filter((info: PageInfo) => info.path === pathname())[0])
   const isNotFoundPage = createMemo(() => !currentPageInfo())
   const isTopPage = createMemo(() => {
-    return pathname() === pagesInfo.filter((info) => info.name === 'top')[0].path
+    return pathname() === pagesInfo.filter((info: PageInfo) => info.name === 'top')[0].path
   })
 
   return (
@@ -23,7 +23,7 @@ function App() {
       <Show when={!isNotFoundPage()} fallback={<Routes />}>
         <KeyVisual isActive={!isTopPage()} />
         <div class={styles.section}>
-          <SectionHeader backgroundImage={currentPageInfo().headerBackGroundImage} title={currentPageInfo().title} />
+          <SectionHeader page={currentPageInfo()} />
           <Routes />
         </div>
       </Show>
