6e4bac3d0016d586cd00bd914384b7051dc653fd
MihiroH <mihiro.hashimoto.1125@gmail.com>
2022-09-24 18:48:05 +0900

    feat: 404ページのコーディング

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,20 @@
+import Image from '@/components/Image/Image'
 import styles from '@/pages/NotFound/NotFound.module.scss'
 
 const NotFound = () => {
-  return <div class={styles.not_found}>NotFound</div>
+  return (
+    <div class={styles.root}>
+      <div class={styles.logo}>
+        <Image src="/img/logo-sub.svg" alt="" width="33" height="33" />
+      </div>
+      <p class={styles.title}>404</p>
+      <p class={styles.text}>
+        お探しのページが見つかりません。
+        <br />
+        移動もしくは削除された可能性があります。
+      </p>
+    </div>
+  )
 }
 
 export default NotFound
